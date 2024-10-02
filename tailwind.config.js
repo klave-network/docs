d94b3f1a4987120d667364e69a8163918406befb
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-10-02 12:55:32 +0200

    chore: Setup shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-import { createPreset, presets } from 'fumadocs-ui/tailwind-plugin';
+import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: ['class'],
 	content: [
 		'./components/**/*.{ts,tsx}',
 		'./app/**/*.{ts,tsx}',
@@ -9,23 +10,57 @@ export default {
 		'./mdx-components.{ts,tsx}',
 		'./node_modules/fumadocs-ui/dist/**/*.js'
 	],
-	presets: [
-		createPreset({
-			preset: {
-				...presets.default,
-				dark: {
-					...presets.default.dark,
-					background: '0 0% 2%',
-					foreground: '0 0% 98%',
-					popover: '0 0% 4%',
-					card: '0 0% 4%',
-					muted: '0 0% 8%',
-					border: '0 0% 14%',
-					accent: '0 0% 15%',
-					'accent-foreground': '0 0% 100%',
-					'muted-foreground': '0 0% 60%'
+	presets: [createPreset()],
+	plugins: [require('tailwindcss-animate')],
+	theme: {
+		extend: {
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			colors: {
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				chart: {
+					1: 'hsl(var(--chart-1))',
+					2: 'hsl(var(--chart-2))',
+					3: 'hsl(var(--chart-3))',
+					4: 'hsl(var(--chart-4))',
+					5: 'hsl(var(--chart-5))'
 				}
 			}
-		})
-	]
+		}
+	}
 };
