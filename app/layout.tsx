f50d2662b091f33ed7e2166347cb6da0facc872e
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-10-10 14:57:09 +0200

    chore: Add favicon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,14 @@ const inter = Inter({
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
+			<head>
+				<link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+				<link rel="shortcut icon" href="/favicon.ico" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<meta name="apple-mobile-web-app-title" content="Klave" />
+				<link rel="manifest" href="/site.webmanifest" />
+			</head>
 			<body className="flex min-h-screen flex-col">
 				<RootProvider>
 					<div className="hidden md:block">
