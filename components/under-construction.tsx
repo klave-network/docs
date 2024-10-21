a1f5789f7521acba291507de2dcf7c573a720722
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-10-21 11:54:59 +0200

    chore: Update dark mode color palette

--- components/under-construction.tsx
+++ components/under-construction.tsx
@@ -2,7 +2,7 @@ import { Construction } from 'lucide-react';
 
 export const UnderConstructionCallout = () => {
 	return (
-		<div className="rounded-lg bg-yellow-100 border border-yellow-400 p-3 flex gap-2 shadow-md">
+		<div className="rounded-lg bg-yellow-100 border border-yellow-400 dark:border-yellow-500 dark:bg-yellow-200 p-3 flex gap-2 shadow-md text-black">
 			<Construction />
 			The docs for this page are under construction.
 		</div>
