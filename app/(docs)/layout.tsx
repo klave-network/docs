363377a007621d4acf744eb84959ed0975751101
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-11-04 16:35:06 +0100

    chore: Move layout config to its own dedicated file

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -1,6 +1,6 @@
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
-import { docsLayoutOptions } from '~/utils/layout-options';
+import { docsLayoutOptions } from '~/app/layout.config';
 
 export default function Layout({ children }: { children: ReactNode }): JSX.Element {
 	return <DocsLayout {...docsLayoutOptions}>{children}</DocsLayout>
