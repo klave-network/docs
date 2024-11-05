d7b023e24541118311b7e190c0102cc41bd58977
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-11-05 15:01:47 +0100

    chore: Add '/' link to logo component

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import KlaveLogoLight from '~/public/images/klave-light.svg';
 import KlaveLogoDark from '~/public/images/klave-dark.svg';
 
 export const Logo = () => {
 	return (
-		<>
+		<Link href="/">
 			<Image src={KlaveLogoLight} alt="Klave logo" width={100} className="hidden dark:block" />
 			<Image src={KlaveLogoDark} alt="Klave logo" width={100} className="dark:hidden block" />
-		</>
+		</Link>
 	);
 };
