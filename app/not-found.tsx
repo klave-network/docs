ffaa6c0cbc10c5fae90500c6ca81f0c6f181e914
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-11-07 12:27:11 +0100

    chore: Create custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import { Button } from '~/components/ui/button';
+import { Logo } from '~/components/logo';
+
+export default function NotFound() {
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-6">
+      <Logo />
+      <div className="flex items-center gap-4">
+        <h1 className="text-2xl">404</h1>
+        <span>Page not found</span>
+      </div>
+      <Link href="/">
+        <Button>Return Home</Button>
+      </Link>
+    </div>
+  )
+}
