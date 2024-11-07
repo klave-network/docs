02cb84e89e034db4f15d414a38b94be62121ab10
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-11-07 11:15:23 +0100

    chore: Remove bg from doc pages

--- app/(docs)/[[...slug]]/page.tsx
+++ app/(docs)/[[...slug]]/page.tsx
@@ -56,7 +56,7 @@ export default async function Page(props: {
 					}}
 				/>
 			</DocsBody>
-			<div className="absolute inset-0 z-[-1] overflow-hidden duration-1000 animate-in fade-in [perspective:2000px]">
+			{/* <div className="absolute inset-0 z-[-1] overflow-hidden duration-1000 animate-in fade-in [perspective:2000px]">
         <div
             className="absolute bottom-[10%] left-1/2 size-[1200px] origin-bottom bg-fd-primary/30 opacity-20"
             style={{
@@ -65,7 +65,7 @@ export default async function Page(props: {
                 'radial-gradient(50% 50% at center,transparent,hsl(var(--background))), repeating-linear-gradient(to right,hsl(var(--primary)),hsl(var(--primary)) 1px,transparent 2px,transparent 100px), repeating-linear-gradient(to bottom,hsl(var(--primary)),hsl(var(--primary)) 2px,transparent 3px,transparent 100px)',
             }}
         />
-      </div>
+      </div> */}
 		</DocsPage>
 	);
 }
