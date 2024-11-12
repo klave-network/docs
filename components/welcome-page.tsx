79e9e33942e1df2a3b166c612b3e544aafc962b1
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2024-11-12 08:17:15 +0100

    chore: Hide 'crypto' chapter for now, add SDK tile to welcome page

--- components/welcome-page.tsx
+++ components/welcome-page.tsx
@@ -1,4 +1,4 @@
-import { Rss, GraduationCap, Store, BookOpenText } from 'lucide-react';
+import { Rss, GraduationCap, Store, BookOpenText, Library } from 'lucide-react';
 import Link from 'fumadocs-core/link';
 import { Button } from '~/components/ui/button';
 
@@ -7,7 +7,7 @@ export default function WelcomePage() {
       <div className="flex flex-col gap-8 items-center justify-center text-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-3 gap-4 md:gap-8">
           <div
-            className="bg-cta-gradient sm:col-span-2 sm:row-span-2 flex flex-col gap-4 justify-center rounded-lg border border-fd-primary/30 bg-transparent p-4 md:px-6 shadow-md"
+            className="bg-cta-gradient sm:col-span-2 sm:row-span-2 flex flex-col gap-4 justify-center rounded-lg border border-fd-primary/30 bg-transparent p-4 md:p-6 shadow-md"
           >
             <div className="flex justify-between items-center">
               <span className="text-2xl sm:text-4xl font-owners font-medium tracking-wide">Quickstart Guide</span>
@@ -25,7 +25,7 @@ export default function WelcomePage() {
             <Link
               key={block.title}
               href={block.link}
-              className="no-underline flex flex-col rounded-lg border border-fd-primary/30 bg-fd-card p-4 md:px-6 shadow-md transition-all hover:border-fd-primary hover:bg-fd-primary/20"
+              className="no-underline flex flex-col rounded-lg border border-fd-primary/30 bg-fd-card p-4 md:p-6 shadow-md transition-all hover:border-fd-primary hover:bg-fd-primary/20"
             >
               <div className="flex justify-between items-center">
                 <span className="text-2xl font-owners font-medium tracking-wide">{block.title}</span>
@@ -58,16 +58,28 @@ const blocks = [
     icon: <Store />
   },
   {
+    title: 'SDK',
+    description: 'Find out how to use Klave SDK to create your first application.',
+    link: '/sdk/latest',
+    icon: <Library />
+  },
+  {
     title: 'Whitepaper',
     description: 'Deep dive into Klave\'s technology and its applications.',
     link: '/whitepaper',
     icon: <BookOpenText />
-    },
+  },
   {
     title: 'Learn',
     description: 'Learn more about Klave and its architecture.',
     link: '/learn/architecture',
     icon: <GraduationCap />
+  },
+  {
+    title: 'Newsletter',
+    description: 'Get short, sweet and brief product updates, company news, and more.',
+    link: 'https://secretarium.com/newsletter',
+    icon: <Rss />
   },
   {
     title: 'GitHub',
@@ -84,12 +96,6 @@ const blocks = [
     </svg>
   },
   {
-    title: 'Newsletter',
-    description: 'Get short, sweet and brief product updates, company news, and more.',
-    link: 'https://secretarium.com/newsletter',
-    icon: <Rss />
-  },
-  {
     title: 'Join our Discord',
     description: 'Join the Klave community and connect with other developers.',
     link: 'https://discord.gg/MkUxsVeqDW',
@@ -102,18 +108,18 @@ const blocks = [
 				>
 					<path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z" />
 				</svg>
-  },
-  {
-    title: 'Follow on X',
-    description: 'Stay up to date with the latest news and updates from Klave.',
-    link: 'https://x.com/KlaveNetwork',
-    icon: <svg
-          role="img"
-					className="size-6"
-					fill="none"
-          viewBox="0 0 1200 1227"
-          xmlns="http://www.w3.org/2000/svg">
-            <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" fill="currentColor"/>
-    </svg>
   }
+  // {
+  //   title: 'Follow on X',
+  //   description: 'Stay up to date with the latest news and updates from Klave.',
+  //   link: 'https://x.com/KlaveNetwork',
+  //   icon: <svg
+  //         role="img"
+		// 			className="size-6"
+		// 			fill="none"
+  //         viewBox="0 0 1200 1227"
+  //         xmlns="http://www.w3.org/2000/svg">
+  //           <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" fill="currentColor"/>
+  //   </svg>
+  // }
 ];
