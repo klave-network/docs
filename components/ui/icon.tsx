c421f96c4a5a1cd7f86b924effc0a146516daf1c
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-02-13 17:31:33 +0100

    chore: Create lintstaged file

--- components/ui/icon.tsx
+++ components/ui/icon.tsx
@@ -4,20 +4,20 @@ import { type HTMLAttributes } from 'react';
 import { cn } from '~/lib/utils';
 
 export function IconContainer({
-  icon: Icon,
-  ...props
+    icon: Icon,
+    ...props
 }: HTMLAttributes<HTMLDivElement> & {
-  icon?: LucideIcon;
+    icon?: LucideIcon;
 }): React.ReactElement {
-  return (
-    <div
-      {...props}
-      className={cn(
-        'rounded-md border bg-gradient-to-b from-muted to-secondary p-0.5 [a[data-active=true]_&]:from-primary/30 [a[data-active=true]_&]:to-primary [a[data-active=true]_&]:text-primary-foreground',
-        props.className,
-      )}
-    >
-      {Icon ? <Icon /> : <TerminalIcon />}
-    </div>
-  );
+    return (
+        <div
+            {...props}
+            className={cn(
+                'rounded-md border bg-gradient-to-b from-muted to-secondary p-0.5 [a[data-active=true]_&]:from-primary/30 [a[data-active=true]_&]:to-primary [a[data-active=true]_&]:text-primary-foreground',
+                props.className
+            )}
+        >
+            {Icon ? <Icon /> : <TerminalIcon />}
+        </div>
+    );
 }
