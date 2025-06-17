50b0dd89dc1d6cb2bb5dc21e3904a63b53e68915
Florian Guitton <florian@secretarium.com>
2025-06-17 22:49:03 +0200

    chore: Bake in the commit number for reference

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,9 @@
 export const Footer = () => {
 	return (
-		<footer className="bg-blue-200 dark:bg-blue-900 text-center text-sm h-56">
-			<p className="text-slate-500 dark:text-slate-300 text-xs">
-				© {new Date().getFullYear()} Copyright
+		<footer className="w-full text-left text-sm">
+			<p className="text-slate-500 dark:text-slate-300 text-xs opacity-20">
+				<a className="text-klave-dark-blue dark:text-klave-light-blue hover:underline pr-0" href="https://secretarium.com/">Secretarium</a> © {new Date().getFullYear()} All rights reserved,&nbsp;
+				<i title={process.env.NEXT_REPO_BRANCH}>v{process.env.NEXT_REPO_VERSION} ({process.env.NEXT_REPO_COMMIT?.substring(0, 8)}{process.env.NEXT_REPO_DIRTY ? <i title='Dirty'>*</i> : ''})</i>
 			</p>
 		</footer>
 	);
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
 import { createMDX } from 'fumadocs-mdx/next';
+import { createRequire } from 'module';
 
+const git = createRequire(import.meta.url)('git-rev-sync');
+const { version } = createRequire(import.meta.url)('./package.json');
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
     output: 'standalone',
     reactStrictMode: true,
+    env: {
+        NEXT_REPO_COMMIT: git.long(),
+        NEXT_REPO_BRANCH: git.branch(),
+        NEXT_REPO_DIRTY: `${git.isDirty()}`,
+        NEXT_REPO_VERSION: version
+    },
     webpack: config => {
         config.resolve.fallback = {
             fs: false,
